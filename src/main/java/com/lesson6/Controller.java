b0de1499b0e6c77d035d015a227cd09e2bd29fb5
Vitaliy <hladon@meta.ua>
2019-06-26 18:20:57 +0300

    1

--- src/main/java/com/lesson6/Controller.java
+++ src/main/java/com/lesson6/Controller.java
@@ -3,15 +3,21 @@ package com.lesson6;
 import com.lesson6.model.Filter;
 import com.lesson6.model.Flight;
 import com.lesson6.model.Plane;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @org.springframework.stereotype.Controller
 public class Controller {
+
+    @Autowired
+    private Service service;
+
     public List<Flight> flightByDate(Filter filter){
 
     }
-    public List<Plane> oldPlanes(){
 
+    public List<Plane> oldPlanes(){
+        return service.oldPlanes();
     }
 }
--- src/main/java/com/lesson6/Service.java
+++ src/main/java/com/lesson6/Service.java
@@ -4,10 +4,9 @@ import com.lesson6.model.Plane;
 import com.lesson6.repository.PlaneDao;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+
 
 public class Service {
 
@@ -16,13 +15,16 @@ public class Service {
 
     public List<Plane> oldPlanes(){
         List<Plane> planeList=planeDao.selectAll();
-        List<Plane> planes;
-        LocalDate localDate=LocalDate.now();
-        LocalDate dateTwentyEgo=localDate.minusYears(20);
+        List<Plane> planes=null;
+        long lifeTime=20*315569250*100;// 20 years in miliseconds
         for(Plane plane:planeList){
             long diff=plane.getYearProduced().getTime()-new Date().getTime();
-            if(TimeUnit.DAYS.convert((plane.getYearProduced().getTime()-new Date().getTime()),TimeUnit.MILLISECONDS)))
-
+            if(diff>lifeTime){
+                planes.add(plane);
+            }
         }
+        return planes;
     }
+
+
 }
